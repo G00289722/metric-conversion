edebe1cbe9d61e22230c68fd5b974000e17707b5
G00289722 <35769288+G00289722@users.noreply.github.com>
2018-04-26 15:50:45 +0100

    all conversions added and working

--- js/convert.js
+++ js/convert.js
@@ -1,3 +1,4 @@
+// feet to meters
 function fToM(){
 
   var measure = parseInt(document.getElementById("value1").value);
@@ -6,6 +7,50 @@ function fToM(){
   var message = measure + ' feet converts to ' + meters + ' meters.';
   console.log(message);
   document.getElementById("resultsentence").innerHTML = message;
+  
+  
+
+}
+
+//inches to centimeters
+function CenT(){
+
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var Centimeters = measure * 2.54;
+  var message = measure + ' inches converts to ' + Centimeters + ' Centimeters.';
+  console.log(message);
+  document.getElementById("resultsentencec").innerHTML = message;
+  
+  
+
+}
+
+//yards to meters
+function YtM(){
+
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var yards = measure * 0.9144;
+  var message = measure + ' yards converts to ' + yards + ' meters.';
+  console.log(message);
+  document.getElementById("resultsentencey").innerHTML = message;
+  
+  
+
+}
+
+//miles to kilometers
+
+function MtK(){
+
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var kilomiles = measure * 1.60934;
+  var message = measure + ' miles converts to ' + kilomiles + ' kilometers.';
+  console.log(message);
+  document.getElementById("resultsentencem").innerHTML = message;
+  
   
 
 }
